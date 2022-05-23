7035188f0a5614df232466548be0ba6dd73b2818
Артем <WWW_cool@inbox.ru>
2022-05-23 13:35:43 +0300

    added comment

--- apps/wapi/test/wapi_tests_SUITE.erl
+++ apps/wapi/test/wapi_tests_SUITE.erl
@@ -279,6 +279,12 @@ make_mapped_error(Name, Type, Desc) ->
     Format = <<"{\"description\":\"Request parameter: ~s, error type: ~s~s\",\"errorType\":\"~s\",\"name\":\"~s\"}">>,
     genlib:to_binary(io_lib:format(Format, [Name, Type, Desc, Type, Name])).
 
+%% Dirty hack to mock private get_request_spec function of swag_server_wallet_w2_w_handler module
+%% by mocking highlevel handle_request_json function (implemeptetion of handle_request_json and state record
+%% copied from swag_server_wallet_w2_w_handler).
+%% If generated code changes in unsupported way we can drop this hack
+%% and use spec rules from generated code and dont cover all test cases, only possible.
+
 mock_spec(Rules) ->
     meck:expect(
         swag_server_wallet_w2_w_handler,
